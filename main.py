0a2dbb0157dbc402f6d2cec7ef894dadedf4f42e
rathoreAbhi0208 <awikumar830@gmail.com>
2022-11-29 02:09:38 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-import pymongo
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.utils import get_collection_as_dataframe
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
+import os,sys
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-# Database Name
-dataBase = client["neurolabDB"]
+# def test_logger_and_exception():
+#      try:
+#           logging.info("Starting the test_logger_and_exception")
+#           result = 3/0
+#           print(result)
+#           logging.info("Stopping test_logger_and_exception")
+#      except Exception as e:
+#           logging.debug(str(e))
+#           raise SensorException(e, sys)
+     
 
-
-# Collection  Name
-collection = dataBase['Products']
-
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+if __name__ =="__main__":
+     try:
+          training_pipeline_config =config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
+     except Exception as e:
+          print(e)
