cbb7a5530d9f25220ac0806de0776c785c64986b
rathoreAbhi0208 <awikumar830@gmail.com>
2022-12-07 23:30:37 +0530

    Completed Data Validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sensor.utils import get_collection_as_dataframe
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 import os,sys
+from sensor.components.data_validation import DataValidation
 
 
 # def test_logger_and_exception():
@@ -23,6 +24,15 @@ if __name__ =="__main__":
           data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
           data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+          
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation =DataValidation(data_validation_config=data_validation_config,
+           data_ingestion_artifact=data_ingestion_artifact)
+
+          data_validation_artifact = data_validation.initiate_data_validation()
+
+
+
      except Exception as e:
           print(e)
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -24,7 +24,7 @@ class DataIngestionConfig:
             self.database_name = 'aps'
             self.collection_name = 'sensor'
             self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
-            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature",FILE_NAME)
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
             self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
             self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
             self.test_size = 0.2
@@ -42,7 +42,15 @@ class DataIngestionConfig:
         
     
 
-class DataValidationConfig:...
+class DataValidationConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir,"data_validation")
+        self.report_file_path = os.path.join(self.data_validation_dir,"report.yaml")
+        self.missing_threshold:float = 0.2
+        self.base_file_path = os.path.join("/config/workspace/aps_failure_training_set1.csv")
+
+        
 class DataTransformationConfig:...
 class DtaTrainerConfig:...
 class DataEvaluationConfig:...
